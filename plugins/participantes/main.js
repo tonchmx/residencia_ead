9cb4d16f50f9d8a6e3d88afa790041e0c9bb1550
tonchmx <tonchmx@hotmail.com>
2013-11-20 22:51:53 -0600

    #9 Participantes
    Creado el plugin de Participantes (éste muestra únicamente la lista de
    participantes de un curso).

--- plugins/participantes/main.js
+++ plugins/participantes/main.js
@@ -1,4 +1,8 @@
-define(function(){
+var templates = [
+    "root/externallib/text!root/plugins/participantes/participantes.html"
+];
+
+define(templates, function(participantesTpl){
     var plugin = {
         settings: {
             name: "participantes",
@@ -18,7 +22,47 @@ define(function(){
         routes: [
             ["participantes/:courseId", "participantes", "mostrarParticipantes"],
             ["participante/:courseId/:userId", "participantes", "mostrarParticipante"]
-        ]
+        ],
+
+        mostrarParticipantes: function(courseId){
+            MM.log("Mostrando participantes");
+            $.mobile.loading( 'show', {
+                text: 'Cargando',
+                textVisible: true,
+                theme: 'b'
+            });
+
+            var data = {
+                "courseid": courseId
+            };
+
+            MM.moodleWSCall('moodle_user_get_users_by_courseid', data, function(users){
+                $.mobile.hidePageLoadingMsg();
+                //cargamos el template
+                var course = MM.db.get("courses", MM.config.current_site.id + "-" + courseId);
+                var pageTitle = "Alumnos de " + course.get("fullname");
+                var variables = {usuarios: users, deviceType: MM.deviceType, courseId: courseId, titulo: pageTitle};
+                var output = MM.tpl.render(MM.plugins.participantes.templates.participantes.html, variables);
+
+                //ocultamos el menu
+                $('#panel-izq').css('display', 'none');
+                $('#panel-centro').css('display', 'block');
+                $('#panel-centro').html(output).trigger("create");
+            }, null, function(m){
+                MM.log("Error participantes");
+                $.mobile.hidePageLoadingMsg();
+            });
+        },
+
+        mostrarParticipante: function(){
+            MM.log("Mostrando participante");
+        },
+
+        templates: {
+            "participantes":{
+                html: participantesTpl
+            }
+        }
     }
 
     MM.registrarPlugin(plugin);
